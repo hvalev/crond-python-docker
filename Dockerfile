6ce4197013d846852747eab95c24adada4a5a655
hvalev <h_valev@yahoo.de>
2020-12-21 22:10:34 +0200

    init2

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,6 @@
-FROM python:rc-alpine
-RUN echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories
-RUN apk update && apk add py3-pip py3-tornado py3-cryptography py3-telegram-bot rsync curl gcc libc-dev libffi-dev openssl-dev chromium chromium-chromedriver --no-cache
+FROM python:3.9-alpine
+RUN apk update && apk add rsync curl gcc chromium chromium-chromedriver --no-cache
 COPY . /.
 RUN pip3 install --upgrade pip
 RUN pip3 install -r requirements.txt
-RUN mkdir data
-RUN mkdir scripts
 RUN /startup.sh
--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-
 echo "Starting startup.sh.."
-echo "*       *       *       *       *       run-parts /etc/periodic/1min" >> /etc/crontabs/root
-echo "*/5       *       *       *       *       run-parts /etc/periodic/5min" >> /etc/crontabs/root
-echo "*/30       *       *       *       *       run-parts /etc/periodic/30min" >> /etc/crontabs/root
+echo "*       *       *       *       *       run-parts /etc/periodic/1min"  >> /etc/crontabs/root
+echo "*/5     *       *       *       *       run-parts /etc/periodic/5min"  >> /etc/crontabs/root
+echo "*/30    *       *       *       *       run-parts /etc/periodic/30min" >> /etc/crontabs/root
+echo "0 	  */6 	  * 	  * 	  *       run-parts /etc/periodic/6hour" >> /etc/crontabs/root
 crontab -l
